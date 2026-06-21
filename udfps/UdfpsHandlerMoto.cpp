moto-sm8450/android_hardware_motorola

motorola: Add udfps handler lib
Change-Id: Ib837e4a70015dc24d431458a1cce5b86e87816a9

udfps/UdfpsHandlerMoto.cpp
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHandler.moto"
+
+#include <android-base/logging.h>
+#include <com/motorola/hardware/biometric/fingerprint/1.0/IMotoFingerPrint.h>
+
+#include <fcntl.h>
+#include <fstream>
+#include <poll.h>
+#include <thread>
+
+#include "UdfpsHandler.h"
+
+// Touchscreen and HBM
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+using ::android::sp;
+using ::android::hardware::hidl_vec;
+using ::com::motorola::hardware::biometric::fingerprint::V1_0::IMotFodEventResult;
+using ::com::motorola::hardware::biometric::fingerprint::V1_0::IMotFodEventType;
+using ::com::motorola::hardware::biometric::fingerprint::V1_0::IMotoFingerPrint;
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+class MotoUdfpsHander : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t* /*device*/) {
+        mMotoFingerprint = IMotoFingerPrint::getService();
+
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fd;
+                return;
+            }
+
+            struct pollfd fodUiPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+                mMotoFingerprint->sendFodEvent(
+                        readBool(fd) ? IMotFodEventType::FINGER_DOWN
+                                     : IMotFodEventType::FINGER_UP,
+                        {}, [](IMotFodEventResult /* result */, const hidl_vec<signed char>& /* data */) {});
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        // nothing
+    }
+
+    void onFingerUp() {
+        // nothing
+    }
+
+  private:
+    sp<IMotoFingerPrint> mMotoFingerprint;
+};
+
+static UdfpsHandler* create() {
+    return new MotoUdfpsHander();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+        .create = create,
+        .destroy = destroy,
+};

Dubai-Resources/hardware_motorola

udfps/UdfpsHandlerMoto.cpp
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHandler.moto"
+
+#include <android-base/logging.h>
+#include <com/motorola/hardware/biometric/fingerprint/1.0/IMotoFingerPrint.h>
+
+#include <fcntl.h>
+#include <poll.h>
+#include <fstream>
+#include <thread>
+
+#include "UdfpsHandler.h"
+
+// Touchscreen and HBM
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+using ::android::sp;
+using ::android::hardware::hidl_vec;
+using ::com::motorola::hardware::biometric::fingerprint::V1_0::IMotFodEventResult;
+using ::com::motorola::hardware::biometric::fingerprint::V1_0::IMotFodEventType;
+using ::com::motorola::hardware::biometric::fingerprint::V1_0::IMotoFingerPrint;
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+class MotoUdfpsHandler : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t* /*device*/) {
+        mMotoFingerprint = IMotoFingerPrint::getService();
+
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fd;
+                return;
+            }
+
+            struct pollfd fodUiPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+                mMotoFingerprint->sendFodEvent(
+                        readBool(fd) ? IMotFodEventType::FINGER_DOWN : IMotFodEventType::FINGER_UP,
+                        {},
+                        [](IMotFodEventResult /* result */,
+                           const hidl_vec<signed char>& /* data */) {});
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        // nothing
+    }
+
+    void onFingerUp() {
+        // nothing
+    }
+
+  private:
+    sp<IMotoFingerPrint> mMotoFingerprint;
+};
+
+static UdfpsHandler* create() {
+    return new MotoUdfpsHandler();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+        .create = create,
+        .destroy = destroy,
+};